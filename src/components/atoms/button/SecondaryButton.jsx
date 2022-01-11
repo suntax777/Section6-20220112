c523416248d9dec4252ad4eef8f6a83a9bacf06b
suntax777 <43720142+suntax777@users.noreply.github.com>
2022-01-12 08:12:12 +0900

    Section6-28-ontheway

---
+++ src/components/atoms/button/SecondaryButton.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import { PrimaryButton } from "./PrimaryButton";
+
+export const SecondaryButton = (props) => {
+  const { children } = props;
+  return <SButton>{children}</SButton>;
+};
+
+const SButton = styled.button`
+  background-color: #11999e;
+  color: #fff;
+  padding: 6px 24px;
+  border: none;
+  border-radius: 9999px;
+  outline: none;
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+  }
+`;
